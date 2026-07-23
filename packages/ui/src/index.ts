fddac992d25221be4cc9daf6ff10cd7a70ddcb10
0x70da <mahmoudAnonymous0@gmail.com>
2026-07-23 19:22:31 +0300

    updating type aliases and tsconfig configuration to avoid errors

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,6 @@
 export * from "./lib/utils";
 export * from "./components/ui/button";
-export * from "./components/ui/card";
+export * from "./components/ui/dropdown-menu";
+export * from "./components/ui/dialog";
+export * from "./components/ui/input";
+export * from "./components/ui/alert-dialog";
