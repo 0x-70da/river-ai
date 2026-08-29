b4916e0e6d9bc6caac97abf55d836182fdaf80b2
0x70da <mahmoudAnonymous0@gmail.com>
2026-08-29 16:35:10 +0300

    Add model controller, and refactor routes for better clarity

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import chatRoutes from "./routes/chat.routes.js";
+import router from "./routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
@@ -13,7 +13,7 @@ app.use(
 
 app.use(express.json());
 
-app.use("/api/chats", chatRoutes);
+app.use("/api", router);
 
 app.use(errorHandler);
 
